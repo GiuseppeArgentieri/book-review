10175c2238ebc3f868b6922f8ebde87850488794
GiuseppeArgentieri <giuseppe.arge24@outlook.it>
2024-07-28 13:11:09 +0200

    make review and rating fillable

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['review', 'rating'];
+    // $fillable -> in order to create reviews like this:
+    // $book = \App\Models\Book::find(1);
+    // $review = $book->reviews()->create(['review'=>'sample review', 'rating'=>5]);
     public function book(){
         return $this->belongsTo(Book::class);
     }
